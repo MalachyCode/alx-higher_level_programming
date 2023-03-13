2515f1cbe38c726380157aa9c623b8fe0de9df06
Malachy Nwafor <nwaformalachy2@gmail.com>
2023-03-13 16:03:58 -0700

    Completed tasks

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
-#!/bin/node
-console.log("C is fun")
-console.log("Python is cool")
-console.log("JavaScript ia amazing")
-
+#!/usr/bin/node
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
